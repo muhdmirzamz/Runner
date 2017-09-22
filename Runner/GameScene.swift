b44ecf6cd7f18c8a20d6501ff5db98945257698c
muhdmirzamz <muhdmirzamz@gmail.com>
2017-09-22 15:28:55 +0800

    Fixed collision bug

--- Runner/GameScene.swift
+++ Runner/GameScene.swift
@@ -22,14 +22,20 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
 	// a way to access the block to test if block has passed player
 	var shapeArr = [SKShapeNode]()
 	
+	var topCollisionBlock: SKShapeNode?
+	var bottomCollisionBlock: SKShapeNode?
+	
 	// interesting to note:
 	// https://stackoverflow.com/questions/21505660/physics-bodies-not-responding-to-bitmask-settings#comment60916704_21508132
-	// do not compare 0 or UInt32.max
-	// it ain't gonna work
+	// do not use 0 or UInt32.max
+	// comparing with 0's will give a zero value
+	// only non-zero values cause collision
 	struct Collision {
 		let Border: UInt32 = 1
 		let Player: UInt32 = 2
 		let Block: UInt32 = 3
+		let TopBlock: UInt32 = 4
+		let BottomBlock: UInt32 = 5
 	}
 	
     override func didMove(to view: SKView) {
@@ -46,34 +52,59 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
 		self.scoreLabel?.zPosition = 3
 		self.addChild(self.scoreLabel!)
 	
-//		self.backgroundImage.size = CGSize.init(width: self.size.width, height: self.size.height)
-//		self.backgroundImage.position = CGPoint.init(x: self.size.width / 2, y: self.size.height / 2)
-//		self.addChild(self.backgroundImage)
-//		
-//		self.backgroundImage2.size = CGSize.init(width: self.size.width, height: self.size.height)
-//		self.backgroundImage2.position = CGPoint.init(x: (self.size.width / 2) + self.size.width , y: self.size.height / 2)
-//		self.addChild(self.backgroundImage2)
+		self.backgroundImage.size = CGSize.init(width: self.size.width, height: self.size.height)
+		self.backgroundImage.position = CGPoint.init(x: self.size.width / 2, y: self.size.height / 2)
+		self.addChild(self.backgroundImage)
+		
+		self.backgroundImage2.size = CGSize.init(width: self.size.width, height: self.size.height)
+		self.backgroundImage2.position = CGPoint.init(x: (self.size.width / 2) + self.size.width , y: self.size.height / 2)
+		self.addChild(self.backgroundImage2)
 	
 		self.player.position = CGPoint.init(x: 50, y: 50)
 		self.player.size = CGSize.init(width: 100, height: 100)
-//		self.player.zPosition = 3
+		self.player.zPosition = 3
 		
 		self.addChild(self.player)
 		
 		self.player.physicsBody = SKPhysicsBody.init(rectangleOf: self.player.size)
 		self.player.physicsBody?.categoryBitMask = Collision().Player
+		self.player.physicsBody?.contactTestBitMask = Collision().Block
 		self.player.physicsBody?.collisionBitMask = Collision().Border
-//		self.player.physicsBody?.contactTestBitMask = Collision().Block
 		
-//		let actionBlock = SKAction.run { 
-//			self.spawnBlock()
-//		}
-//		
-//		let wait = SKAction.wait(forDuration: 4)
-//		let sequence = SKAction.sequence([actionBlock, wait])
-//		let forever = SKAction.repeatForever(sequence)
-//		
-//		run(forever)
+		let actionBlock = SKAction.run { 
+			self.spawnBlock()
+		}
+		
+		let wait = SKAction.wait(forDuration: 4)
+		let sequence = SKAction.sequence([actionBlock, wait])
+		let forever = SKAction.repeatForever(sequence)
+		
+		run(forever)
+		
+		// same size as blocks
+		self.topCollisionBlock = SKShapeNode.init(rectOf: CGSize.init(width: 50, height: 50))
+		self.topCollisionBlock?.isHidden = true
+		self.topCollisionBlock?.fillColor = UIColor.red
+		// point of origin is center
+		self.topCollisionBlock?.position = CGPoint.init(x: 25, y: self.size.height - 25)
+		self.topCollisionBlock?.zPosition = 3
+		self.addChild(self.topCollisionBlock!)
+		
+		self.topCollisionBlock?.physicsBody = SKPhysicsBody.init(rectangleOf: (self.topCollisionBlock?.frame.size)!)
+		self.topCollisionBlock?.physicsBody?.categoryBitMask = Collision().TopBlock
+		self.topCollisionBlock?.physicsBody?.collisionBitMask = 0
+		self.topCollisionBlock?.physicsBody?.contactTestBitMask = Collision().Block
+		
+//		self.bottomCollisionBlock = SKShapeNode.init(rectOf: CGSize.init(width: 50, height: 50))
+//		self.bottomCollisionBlock?.isHidden = true
+//		self.bottomCollisionBlock?.fillColor = UIColor.red
+//		self.bottomCollisionBlock?.position = CGPoint.init(x: self.player.position.x - 20, y: 25)
+//		self.bottomCollisionBlock?.zPosition = 3
+//		self.addChild(self.bottomCollisionBlock!)
+		
+//		self.bottomCollisionBlock?.physicsBody = SKPhysicsBody.init(rectangleOf: (self.bottomCollisionBlock?.frame.size)!)
+//		self.bottomCollisionBlock?.physicsBody?.categoryBitMask = Collision().BottomBlock
+//		self.bottomCollisionBlock?.physicsBody?.contactTestBitMask = Collision().Block
     }
 	
 	func spawnBlock() {
@@ -100,7 +131,8 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
 		
 		block.physicsBody = SKPhysicsBody.init(rectangleOf: block.frame.size)
 		block.physicsBody?.categoryBitMask = Collision().Block
-		block.physicsBody?.contactTestBitMask = Collision().Player
+		block.physicsBody?.contactTestBitMask = Collision().Player | Collision().TopBlock | Collision().BottomBlock
+		block.physicsBody?.isDynamic = false
 		
 		let action = SKAction.move(to: CGPoint.init(x: -100, y: block.position.y), duration: 3)
 		let remove = SKAction.removeFromParent()
@@ -114,6 +146,7 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
 	}
 	
 	public func didBegin(_ contact: SKPhysicsContact) {
+		// player and block
 		if (contact.bodyA.categoryBitMask == Collision().Player && contact.bodyB.categoryBitMask == Collision().Block) || (contact.bodyA.categoryBitMask == Collision().Block && contact.bodyB.categoryBitMask == Collision().Player) {
 			let remove = SKAction.removeFromParent()
 		
@@ -121,6 +154,11 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
 			contact.bodyB.node?.run(remove)
 			
 			self.shapeArr.remove(at: self.shapeArr.startIndex)
+		}
+		
+		if (contact.bodyA.categoryBitMask == Collision().TopBlock && contact.bodyB.categoryBitMask == Collision().Block) || (contact.bodyA.categoryBitMask == Collision().Block && contact.bodyB.categoryBitMask == Collision().TopBlock) {
+			self.score += 1
+			self.scoreLabel?.text = "Score: \(score)"
 		}
 	}
 	
@@ -131,27 +169,16 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
 		
 		self.player.run(moveAction)
 	}
-    
+	
     override func update(_ currentTime: TimeInterval) {
         // Called before each frame is rendered
 		
-//		self.backgroundImage.position.x -= 1
-//		self.backgroundImage2.position.x -= 1
-//		
-//		if self.backgroundImage.position.x + self.backgroundImage.size.width <= (self.size.width / 2) {
-//			self.backgroundImage.position.x = self.size.width / 2
-//			self.backgroundImage2.position.x = (self.size.width / 2) + self.size.width
-//		}
+		self.backgroundImage.position.x -= 1
+		self.backgroundImage2.position.x -= 1
 		
-		if let firstShape = self.shapeArr.first {
-			// okay so you have to round the coords up to an int for it to detect an equality in value
-			// cgfloats just dont do the trick
-			
-			if Int(firstShape.position.x) == Int((self.player.position.x + (self.player.size.width / 2))) {
-				self.score += 1
-				
-				self.scoreLabel?.text = "Score: \(score)"
-			}
+		if self.backgroundImage.position.x + self.backgroundImage.size.width <= (self.size.width / 2) {
+			self.backgroundImage.position.x = self.size.width / 2
+			self.backgroundImage2.position.x = (self.size.width / 2) + self.size.width
 		}
     }
 }
